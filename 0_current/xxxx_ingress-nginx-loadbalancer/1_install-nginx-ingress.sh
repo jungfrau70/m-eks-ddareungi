195fd66acbe9017146538910408a51cf8bd6d9d4
InHwan Jung <jupyteronline@gmail.com>
2020-11-17 22:15:57 +0900

    updated

--- 0_current/xxxx_ingress-nginx-loadbalancer/1_install-nginx-ingress.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source ./.env
-kubectl patch svc default-http-backend -n ingress-nginx -p '{"spec": {"type": "LoadBalancer", "externalIPs":["183.111.236.61"]}}'
--- 0_current/xxxx_ngrok/1_change-dashboard-with-loadbalancer.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec": {"type": "NodePort"}}'
-kubectl patch svc istio-ingressgateway -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
